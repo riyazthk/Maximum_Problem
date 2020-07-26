0168f9f7e3a07a50850e5336f4f7f53030c60137
riyazthk <er.riyaz2507@gmail.com>
2020-07-26 22:40:22 +0530

    [Riyaz]ADDED:To include one additional thing as parameter

--- src/main/java/com/maximum/MaximumProblem.java
+++ src/main/java/com/maximum/MaximumProblem.java
@@ -3,29 +3,51 @@ package com.maximum;
 public class MaximumProblem<E extends Comparable<E>> {
 
     E firstValue;
-    E secondValue;
-    E thirdValue;
+    E[] optional;
 
 
-
-    public MaximumProblem(E firstValue,E secondValue, E thirdValue) {
-        this.firstValue=firstValue;
-        this.secondValue=secondValue;
-        this.thirdValue=thirdValue;
+    public MaximumProblem(E firstValue, E... optional) {
+        this.firstValue = firstValue;
+        this.optional = optional;
     }
 
 
-    public static  <E extends Comparable<E>> E compareValue(E firstValue,E secondValue,E thirdValue){
-        E firstVal=firstValue;
-        if(secondValue.compareTo(firstVal)>0){
-            firstVal=secondValue;
+    public static <E extends Comparable<E>> E compareValue(E firstValue, E... optional) {
+        E max = firstValue;
+        for (E value : optional) {
+            if (value.compareTo(max) > 0) {
+                max = value;
+            }
         }
-        if(thirdValue.compareTo(firstVal)>0){
-            firstVal=thirdValue;
-        }
-        return  firstVal;
+        return max;
     }
-    public  E findMaximum(){
-        return compareValue(firstValue,secondValue,thirdValue);
+
+    public E findMaximum() {
+        return compareValue(firstValue, optional);
+    }
+
+    public static String testMaximum(String x, String y, String z) {
+        String max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
+
+    public static void main(String[] arg, String xstr, String ystr, String zstr) {
+        Integer xInteger = 2, yInteger = 3, zInteger = 4;
+        Float xFloat = 6.6f, yFloat = 8.0f, zFloat = 7.7f;
+        String xString = "pear", yString = "apple", zString = "orange";
+        MaximumProblem.testMaximum(xstr, ystr, zstr);
+        Comparable integerMax = new MaximumProblem(xInteger, yInteger, zInteger).findMaximum();
+        System.out.println(integerMax);
+        Comparable floatMax = new MaximumProblem(xFloat, yFloat, zFloat).findMaximum();
+        System.out.println(floatMax);
+        Comparable StringMax = new MaximumProblem(xString, yString, zString).findMaximum();
+        System.out.println(StringMax);
+
     }
 }
