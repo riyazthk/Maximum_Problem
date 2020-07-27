24456c710fbd5521b57c960f2ffcf0d049b9984f
riyazthk <er.riyaz2507@gmail.com>
2020-07-27 13:13:25 +0530

    [Riyaz]ADDED:To refactor a test cases for optimizing a program

--- src/test/java/com/maximum/MaximumProblemTest.java
+++ src/test/java/com/maximum/MaximumProblemTest.java
@@ -6,56 +6,65 @@ import org.junit.Test;
 public class MaximumProblemTest {
     @Test
     public void toFind_Maximum_Number_PassValue_InThirdParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem(2,3,4);
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals(4, maximum);
+        MaximumProblem<Integer> maximumProblem = new MaximumProblem<Integer>(2,3,4);
+        Integer maximum = maximumProblem.findMaximum();
+        Integer expected=4;
+        Assert.assertEquals(expected, maximum);
     }
     @Test
     public void toFind_Maximum_Number_PassValue_InSecondParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem(2,4);
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals(4, maximum);
+        MaximumProblem<Integer> maximumProblem = new MaximumProblem<Integer>(2,4,3,0);
+        Integer maximum = maximumProblem.findMaximum();
+        Integer expected=4;
+        Assert.assertEquals(expected, maximum);
     }
     @Test
     public void toFind_Maximum_Number_PassValue_InFirstParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem(4,3,2);
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals(4, maximum);
+        MaximumProblem<Integer> maximumProblem = new MaximumProblem<Integer>(4,3,2,5);
+        Integer maximum = maximumProblem.findMaximum();
+        Integer expected=5;
+        Assert.assertEquals(expected, maximum);
     }
     @Test
     public void toFind_Maximum_FloatNumber_PassValue_InFirstParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem(1.4,1.3,1.2);
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals(1.4, maximum);
+        MaximumProblem<Float> maximumProblem = new MaximumProblem<Float>(1.4f,1.3f,1.2f,1.1f);
+        Float maximum = maximumProblem.findMaximum();
+        Float expected=1.4f;
+        Assert.assertEquals(expected, maximum);
     }
     @Test
     public void toFind_Maximum_FloatNumber_PassValue_InSecondParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem(1.2,1.4,1.3);
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals(1.4, maximum);
+        MaximumProblem<Float> maximumProblem = new MaximumProblem<Float>(1.2f,1.4f,1.3f);
+        Float maximum = maximumProblem.findMaximum();
+        Float expected=1.4f;
+        Assert.assertEquals(expected, maximum);
     }
     @Test
     public void toFind_Maximum_FloatNumber_PassValue_InThirdParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem(1.2,1.3,1.4);
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals(1.4, maximum);
+        MaximumProblem<Float> maximumProblem = new MaximumProblem<Float>(1.2f,1.3f,1.4f);
+        Float maximum = maximumProblem.findMaximum();
+        Float expected=1.4f;
+        Assert.assertEquals(expected, maximum);
     }
     @Test
     public void toFind_Maximum_String_PassValue_InFirstParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem("Peach","Apple","Banana");
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals("Peach", maximum);
+        MaximumProblem<String> maximumProblem = new MaximumProblem<String>("Peach","Apple","Banana","Orange");
+        String maximum = maximumProblem.findMaximum();
+        String expected="Peach";
+        Assert.assertEquals(expected, maximum);
     }
     @Test
     public void toFind_Maximum_String_PassValue_InSecondParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem("Apple","Peach","Banana");
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals("Peach", maximum);
+        MaximumProblem<String> maximumProblem = new MaximumProblem<String>("Apple","Carrot","Banana","Orange");
+        String maximum = maximumProblem.findMaximum();
+        String expected="Orange";
+        Assert.assertEquals(expected, maximum);
     }
     @Test
     public void toFind_Maximum_String_PassValue_InThirdParameter() {
-        MaximumProblem maximumProblem = new MaximumProblem("Apple","Banana","Peach");
-        Object maximum = maximumProblem.findMaximum();
-        Assert.assertEquals("Peach", maximum);
+        MaximumProblem<String> maximumProblem = new MaximumProblem<String>("Apple","Banana","Peach");
+        String maximum = maximumProblem.findMaximum();
+        String expected="Peach";
+        Assert.assertEquals(expected, maximum);
     }
 }
