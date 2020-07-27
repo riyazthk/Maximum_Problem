0ba1c4e5d99d2c5d41fa95365a39d4a38cfd2d72
riyazthk <er.riyaz2507@gmail.com>
2020-07-27 13:14:38 +0530

    [Riyaz]ADDED:To refactor a code for a optional values

--- src/main/java/com/maximum/MaximumProblem.java
+++ src/main/java/com/maximum/MaximumProblem.java
@@ -3,51 +3,46 @@ package com.maximum;
 public class MaximumProblem<E extends Comparable<E>> {
 
     E firstValue;
+    E secondValue;
+    E thirdValue;
     E[] optional;
 
 
-    public MaximumProblem(E firstValue, E... optional) {
+    public MaximumProblem(E firstValue, E secondValue, E thirdValue, E... optional) {
         this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
         this.optional = optional;
     }
 
 
-    public static <E extends Comparable<E>> E compareValue(E firstValue, E... optional) {
+    public static <E extends Comparable<E>> E compareValue(E firstValue, E secondValue, E thirdValue, E... optional) {
         E max = firstValue;
-        for (E value : optional) {
-            if (value.compareTo(max) > 0) {
-                max = value;
+
+        if (secondValue.compareTo(max) > 0) {
+            max = secondValue;
+        }
+        if (thirdValue.compareTo(max) > 0) {
+            max = thirdValue;
+        }
+        if (optional != null) {
+            for (E values : optional) {
+                if (values.compareTo(max) > 0) {
+                    max = values;
+                }
             }
         }
+        printMax(max);
         return max;
+    }
+
+    private static <E extends Comparable<E>> void printMax(E max) {
+        System.out.println(max);
     }
 
     public E findMaximum() {
-        return compareValue(firstValue, optional);
+        return compareValue(firstValue, secondValue, thirdValue, optional);
     }
 
-    public static String testMaximum(String x, String y, String z) {
-        String max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
-    }
 
-    public static void main(String[] arg, String xstr, String ystr, String zstr) {
-        Integer xInteger = 2, yInteger = 3, zInteger = 4;
-        Float xFloat = 6.6f, yFloat = 8.0f, zFloat = 7.7f;
-        String xString = "pear", yString = "apple", zString = "orange";
-        MaximumProblem.testMaximum(xstr, ystr, zstr);
-        Comparable integerMax = new MaximumProblem(xInteger, yInteger, zInteger).findMaximum();
-        System.out.println(integerMax);
-        Comparable floatMax = new MaximumProblem(xFloat, yFloat, zFloat).findMaximum();
-        System.out.println(floatMax);
-        Comparable StringMax = new MaximumProblem(xString, yString, zString).findMaximum();
-        System.out.println(StringMax);
-
-    }
 }
